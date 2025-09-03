acbb8888f9b0c7e320f1037420d41deab45b7e00
moipav <moipav@yandex.ru>
2025-09-03 18:46:38 +0800

    добавил миграцию на изменение поользователя

---
+++ database/migrations/2025_09_03_071208_edit_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->after('name', function (Blueprint $table) {
+                $table->string('surname');
+                $table->string('phone', 10);
+            });
+        });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->after('email', function (Blueprint $table) {
+                $table->date('date_of_birth');
+            });
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['surname', 'phone', 'date_of_birth']);
+        });
+    }
+};
