4d7e198571119125345eec38698236f26fb5a1cc
moipav <moipav@yandex.ru>
2025-09-05 11:25:54 +0800

    добавил миграции для указания статуса пользователя + миграции для таблиц, связанных с продуктами

---
+++ database/migrations/2025_09_05_025954_create_product_sizes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_sizes', function (Blueprint $table) {
+            $table->id();
+            $table->integer('product_id');
+            $table->string('size_name', 40);
+            $table->decimal('size_value', 5, 2);
+            $table->string('unit', 10); //ед. измерения
+            $table->decimal('price_adjustment', 10, 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_sizes');
+    }
+};
