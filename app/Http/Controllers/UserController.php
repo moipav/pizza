7d7c2f052f3f0b214f8db190b65b33db96a58a50
moipav <moipav@yandex.ru>
2025-11-26 21:38:57 +0800

    добавил возможность безопасного удаления для Users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,7 +14,7 @@ class UserController extends Controller
      */
     public function index(): View
     {
-        $users = User::all();//
+        $users = User::all();
         return view('users.index', compact('users'));
     }
 
