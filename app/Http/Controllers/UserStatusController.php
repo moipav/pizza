5c9e1828eae4a0a9e0b05c56b3abc90e2b3bbc8f
moipav <moipav@yandex.ru>
2025-12-10 16:58:13 +0800

    Добавил вывод статуса напротив пользователя. начал делать CRUD для UserStatus
    длбавил модель и контроллер для products

---
+++ app/Http/Controllers/UserStatusController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserStatus;
+use Illuminate\Http\Request;
+
+class UserStatusController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $statuses = UserStatus::all();
+        return view('users.statuses.index', compact('statuses'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ app/Models/Product.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'category_id',
+        'name',
+        'description',
+        'price',
+        'image'
+    ];
+
+    protected function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+}
