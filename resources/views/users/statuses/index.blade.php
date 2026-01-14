c82a35e3d755ab9518e5f516631b062d566f28e9
moipav <moipav@yandex.ru>
2026-01-14 21:11:59 +0800

    Приукрасил немного страницу редактирования статусов

--- resources/views/users/statuses/index.blade.php
+++ resources/views/users/statuses/index.blade.php
@@ -2,7 +2,32 @@
 
 @section('content')
     <h1>Доступные статусы:</h1>
-    @foreach($statuses as $status)
-        <p>{{$status->name}}</p>
-    @endforeach
+    <a href="{{ route('statuses.create') }}" class="btn btn-primary">Добавить новый статус</a>
+
+    <table class="table">
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th>Статус</th>
+            <th>Действия</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($statuses as $status)
+            <tr>
+                <td>{{ $status->id }}</td>
+                <td>{{ $status->name }}</td>
+                <td>
+                    <a class="btn btn-primary" href="{{ route('statuses.show', $status) }}">Просмотр</a>
+                    <a class="btn btn-warning" href="{{ route('statuses.edit', $status) }}">Редактировать</a>
+                    <form action="{{ route('statuses.destroy', $status) }}" method="POST" style="display:inline;">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn btn-danger" type="submit" onclick="return confirm('Удалить?')">Удалить</button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
 @endsection
