df06758141bd21cca63c707631f7cfd109edfc1b
moipav <moipav@yandex.ru>
2026-02-11 15:13:58 +0800

    Начал делать фабрики и сиды

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\UserStatus;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -24,8 +25,12 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'name' => fake()->firstName(),
+            'surname' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
+            'phone' => '9' . implode(fake()->unique()->randomElements(range(1, 9),9, true)),
+            'date_of_birth' => fake()->dateTimeBetween('-120 years', '-12 years'),
+            'status_id' => UserStatus::query()->inRandomOrder()->first(),//добавил возможность случайного присвоения статуса
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
