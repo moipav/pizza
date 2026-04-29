79d7e1913b9306a2f5fa8b036a01148c710492c7
moipav <moipav@yandex.ru>
2026-04-29 17:27:42 +0800

    Добавил mergeCart в RegisterController

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,22 +3,33 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Actions\Auth\RegisterUser;
+use App\Actions\Cart\MergeCartWithLogin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\RegisterRequest;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 
 class RegisterController extends Controller
 {
-    public function showRegistrationForm()
+    public function __construct(private readonly MergeCartWithLogin $mergeCartWithLogin)
+    {
+
+    }
+
+
+    public
+    function showRegistrationForm()
     {
         return view('auth.register');
     }
 
-    public function register(RegisterRequest $request, RegisterUser $registerUser)
+    public
+    function register(RegisterRequest $request, RegisterUser $registerUser): RedirectResponse
     {
         $result = $registerUser->execute($request->all());
-
-        Auth::login($result['user']); //атоматический вход после регистрации
+        Auth::login($result); //атоматический вход после регистрации
+        $this->mergeCartWithLogin->execute(Auth::id(), $request->cookie('guestID' ?? $request->session()->getId()));
+        $request->session()->regenerate();
 
         return to_route('home')
             ->with('success', 'Вы успешно зарегестрировались, добро пожаловать!')
